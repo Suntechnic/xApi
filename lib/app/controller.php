031d0fe92330985b3c1eba63440bc79eccd39d4b
Александр Маджугин <madzhugin@gmail.com>
2023-03-21 20:49:30 +0000

    state2303212349

--- lib/app/controller.php
+++ lib/app/controller.php
@@ -9,36 +9,31 @@ BX.ajax.runAction('x:api.app.controller.config')
 namespace X\Api\App;
 
 class Controller extends \App\Api\Controller
-{   
-    protected function init()
-    {
-        parent::init();
-        foreach ($this->actionsConfig as $name=>$arConfig) $this->setActionConfig($name, $arConfig);
-    }
+{       
+    
 }
 
 /*
 namespace App\Api;
 class Controller extends \Bitrix\Main\Engine\Controller
 {
-    
     private $actionsConfig = [
-            //'config' => [
-            //        '-prefilters' => [
-            //                '\Bitrix\Main\Engine\ActionFilter\Authentication'
-            //            ]
-            //    ],
+            'config' => [
+                   '-prefilters' => [
+                           '\Bitrix\Main\Engine\ActionFilter\Authentication'
+                       ]
+               ]
         ];
+
+    protected function init()
+    {
+        parent::init();
+        foreach ($this->actionsConfig as $name=>$arConfig) $this->setActionConfig($name, $arConfig);
+    }
     
     public function configAction ()
     {
-        $selfModule = new \X\Module\Module();
-        return [
-                'assets' => [
-                        'images' => '/bitrix/images/'.$selfModule->MODULE_SPACE.'/'.$selfModule->MODULE_UID
-                    ],
-                'version' => $selfModule->MODULE_VERSION
-            ];
+        return \App\Settings::getConfig();
     }
 }
 */
